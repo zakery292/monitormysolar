3c79806d668b2f1d4749f1c996ad6060f1007c9a
zakery292 <112213249+zakery292@users.noreply.github.com>
2024-08-24 17:22:34 +0100

    Added latest Version and change log sensors

--- custom_components/monitormysolar/const.py
+++ custom_components/monitormysolar/const.py
@@ -128,7 +128,9 @@ ENTITIES = {
         "Lux": {
             "sensors": {
                 "inputbank1": [
-                    {"name:": "Dongle Version", "type": "sensor", "unique_id": "dongle_version"},
+                    {"name:": "Dongle Version", "type": "sensor", "unique_id": "SW_VERSION", "state_class": "text"},
+                    {"name:": "Latest Dongle Version", "type": "sensor", "unique_id": "SW_VERSION", "state_class": "text"},
+
                     {"name": "State", "type": "sensor", "unique_id": "state"},
                     {"name": "Voltage PV1", "type": "sensor", "unique_id": "vpv1", "unit_of_measurement": "V", "device_class": "voltage"},
                     {"name": "Voltage PV2", "type": "sensor", "unique_id": "vpv2", "unit_of_measurement": "V", "state_class": "measurement", "device_class": "voltage"},

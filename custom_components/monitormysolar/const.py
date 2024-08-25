a2bdcdea0217471ff34aa5f571158ce0bc723a40
zakery292 <112213249+zakery292@users.noreply.github.com>
2024-08-25 21:12:33 +0100

    Added unit to house consumption live

--- custom_components/monitormysolar/const.py
+++ custom_components/monitormysolar/const.py
@@ -130,7 +130,7 @@ ENTITIES = {
                 "inputbank1": [
                     {"name": "Dongle Version", "type": "sensor", "unique_id": "SW_VERSION", "state_class": "text"},
                     {"name": "Latest Dongle Version", "type": "sensor", "unique_id": "latestFirmwareVersion", "state_class": "text"},
-                    {"name": "House Consumption (Live)", "type": "sensor", "unique_id": "pload", "state_class": "measurement", "device_class": "wattage", "device_class": "power"},
+                    {"name": "House Consumption (Live)", "type": "sensor", "unique_id": "pload", "state_class": "measurement", "device_class": "power", "unit_of_measurement": "UnitOfPower.WATT" },
                     {"name": "State", "type": "sensor", "unique_id": "state"},
                     {"name": "Voltage PV1", "type": "sensor", "unique_id": "vpv1", "unit_of_measurement": "V", "device_class": "voltage"},
                     {"name": "Voltage PV2", "type": "sensor", "unique_id": "vpv2", "unit_of_measurement": "V", "state_class": "measurement", "device_class": "voltage"},
